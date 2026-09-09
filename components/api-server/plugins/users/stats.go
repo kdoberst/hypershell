88b3683ab85d9cfd8f197e4f5e7c991600e21565
Kim Doberstein <kdoberst@redhat.com>
2026-09-09 16:09:28 -0500

    HYPERSHELL-279 Better user stats

---
+++ components/api-server/plugins/users/stats.go
@@ -0,0 +1,66 @@
+package users
+
+import "time"
+
+const activityLookbackDays = 30
+
+type DailyCount struct {
+	Date  string
+	Count int64
+}
+
+type ActivityStats struct {
+	TotalRegistered      int64
+	RegisteredLast7Days  int64
+	RegisteredLast30Days int64
+	ActiveLast7Days      int64
+	ActiveLast30Days     int64
+	RegistrationDaily    []DailyCount
+	ActiveDaily          []DailyCount
+}
+
+func utcDayStart(value time.Time) time.Time {
+	year, month, day := value.UTC().Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+}
+
+func formatUTCDate(value time.Time) string {
+	return utcDayStart(value).Format("2006-01-02")
+}
+
+func dailySeriesStart(evaluationTime time.Time) time.Time {
+	endDay := utcDayStart(evaluationTime)
+	return endDay.AddDate(0, 0, -(activityLookbackDays - 1))
+}
+
+func registration7DayWindowStart(evaluationTime time.Time) time.Time {
+	return utcDayStart(evaluationTime).AddDate(0, 0, -6)
+}
+
+func registration30DayWindowStart(evaluationTime time.Time) time.Time {
+	return dailySeriesStart(evaluationTime)
+}
+
+func registeredIn7DayWindow(createdAt time.Time, evaluationTime time.Time) bool {
+	return !createdAt.Before(registration7DayWindowStart(evaluationTime))
+}
+
+func registeredIn30DayWindow(createdAt time.Time, evaluationTime time.Time) bool {
+	return !createdAt.Before(registration30DayWindowStart(evaluationTime))
+}
+
+func buildDailySeries(
+	startDay time.Time,
+	endDay time.Time,
+	counts map[string]int64,
+) []DailyCount {
+	series := make([]DailyCount, 0, activityLookbackDays)
+	for day := startDay; !day.After(endDay); day = day.AddDate(0, 0, 1) {
+		date := formatUTCDate(day)
+		series = append(series, DailyCount{
+			Date:  date,
+			Count: counts[date],
+		})
+	}
+	return series
+}
